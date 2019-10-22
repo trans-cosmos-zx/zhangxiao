886405dfcbdd9f3fe08ebef309da6edde6de8135
zhang.xiao <zhang.xiao@trans-cosmos.com.cn>
2019-10-22 11:17:11 +0800

    add str2

--- lianxi2019/src/lianxi2019/ceshi.java
+++ lianxi2019/src/lianxi2019/ceshi.java
@@ -4,7 +4,8 @@ public class ceshi {
 
 	public static void main(String[] args) {
 		String str = "Hello World!";
-		System.out.println(str);
+		String str2="nihao";
+		System.out.println(str+str2);
 	}
 
 }
