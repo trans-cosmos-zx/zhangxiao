a9ee352e92c23c8f48e0dd2112baccefeaff5d94
zhang.xiao <zhang.xiao@trans-cosmos.com.cn>
2019-10-22 15:58:25 +0800

    add method

--- lianxi2019/src/lianxi2019/Add.java
+++ lianxi2019/src/lianxi2019/Add.java
@@ -1,10 +1,20 @@
 package lianxi2019;
 
+import java.util.Scanner;
+
 public class Add {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		int a;
+		int b;
+		Scanner scanner=new Scanner(System.in);
+		System.out.println("请输入第一个加数");
+		a=new Integer(scanner.nextLine());
+		System.out.println("请输入第二个加数");
+		b=new Integer(scanner.nextLine());
+		int c=a+b;
+		System.out.println("a+b="+c);
 	}
 
 }
