dcc01be363fd39264720f57c6607ac6bc183455b
shenxin <shen.xin1@trans-cosmos.com.cn>
2019-10-22 16:13:51 +0800

    cheng

--- lianxi2019/src/lianxi2019/multiply.java
+++ lianxi2019/src/lianxi2019/multiply.java
@@ -1,10 +1,20 @@
 package lianxi2019;
 
+import java.util.Scanner;
+
 public class multiply {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		System.out.println("kandaole");
+		double a;
+		double b;
+		double result;
+		Scanner sc = new Scanner(System.in);
+		System.out.println("请输入第一位数：");
+		a = sc.nextDouble();
+		System.out.println("请输入第二位数：");
+		b = sc.nextInt();
+		result = a * b;
+		System.out.println(result);
 	}
 
 }
