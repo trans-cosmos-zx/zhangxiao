0d11bfe0cfa1b0f6ec28bff3e990261f106f15a4
zhang.xiao <zhang.xiao@trans-cosmos.com.cn>
2019-10-23 11:47:31 +0800

    xinbanben

--- lianxi2019/src/lianxi2019/Add.java
+++ lianxi2019/src/lianxi2019/Add.java
@@ -11,7 +11,7 @@ public class Add {
 		Scanner scanner=new Scanner(System.in);
 		System.out.println("请输入第一个加数");
 		a=new Integer(scanner.nextLine());
-		System.out.println("请输入第二3个加数");
+		System.out.println("请输入第二33个加数");
 		b=new Integer(scanner.nextLine());
 		int c=a+b;
 		System.out.println("a+b="+c);
