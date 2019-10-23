bd951846fcaae8a3b473deeaca28debc2ec60414
shenxin <shen.xin1@trans-cosmos.com.cn>
2019-10-23 15:20:05 +0800

    co

--- lianxi2019/src/lianxi2019/Helloworld.java
+++ lianxi2019/src/lianxi2019/Helloworld.java
@@ -4,7 +4,8 @@ public class Helloworld {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		String str="abc";
+		String str = "abc";
+		String str1 = "abc";
 	}
 
 }
