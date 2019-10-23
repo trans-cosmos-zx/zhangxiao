828c34cabfa4f15f9513b70c1c870208f6948120
zhang.xiao <zhang.xiao@trans-cosmos.com.cn>
2019-10-23 14:34:08 +0800

    me

--- lianxi2019/src/lianxi2019/ceshi.java
+++ lianxi2019/src/lianxi2019/ceshi.java
@@ -5,9 +5,6 @@ public class ceshi {
 	public static void main(String[] args) {
 		String str = "Hello World!";
 		String str1 = "Hello World!";
-		System.out.println(str);
-		System.out.println(str1);
-		System.out.println("nihao!");
 	}
 
 }
